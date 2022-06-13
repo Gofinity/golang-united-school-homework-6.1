5ceeda3967c122a18af5866c98ccdf567604bf90
konami <konami@pop-os.localdomain>
2022-06-13 15:53:01 +0200

    feat: half done"

--- box.go
+++ box.go
@@ -1,5 +1,6 @@
 package golang_united_school_homework
 
+import "fmt"
 
 // box contains list of shapes and able to perform operations on them
 type box struct {
@@ -17,13 +18,22 @@ func NewBox(shapesCapacity int) *box {
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	panic("implement me")
+	err := fmt.Errorf("exceeds shapesCapacity %v", b.shapesCapacity)
+	if len(b.shapes) >= b.shapesCapacity {
+		return err
+	}
+	b.shapes = append(b.shapes, shape)
+	return nil
 }
 
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	panic("implement me")
+	err := fmt.Errorf("exceeds shapesCapacity %v", b.shapesCapacity)
+	if b.shapesCapacity >= i {
+		return nil, err
+	}
+	return b.shapes[i], nil
 
 }
 
--- triangle.go
+++ triangle.go
@@ -1,6 +1,17 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (t Triangle) CalcPerimeter() float64 {
+	return t.Side * 3
+}
+
+func (t Triangle) CalcArea() float64 {
+	var sP float64 = t.Side * 3 * 0.5
+	return math.Sqrt(sP * (sP * t.Side) * (sP * t.Side) * (sP * t.Side))
 }
